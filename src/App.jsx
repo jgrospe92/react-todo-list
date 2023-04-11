4b9565d70fc305f139cd7f4fdcfe73569b4ff5ef
Jeffrey <jeffreygrospe@yahoo.com>
2023-04-10 23:10:03 -0400

    add simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import "./styles.css";
+
 export default function App() {
-  return "Hi"
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">add</button>
+      </form>
+      <h1 className="header">TOdo List</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
